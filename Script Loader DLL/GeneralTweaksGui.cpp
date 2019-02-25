26c01505a886f3134bcfdbd5a4ed4498b59a513b
Moneyl <8206401+Moneyl@users.noreply.github.com>
2019-02-25 17:53:12 -0500

    Added buttons in the tweaks menu to dump rfg::world info
    Can dump general info, the names and positions of all objects, or the names of all world zones.

--- Script Loader DLL/GeneralTweaksGui.cpp
+++ Script Loader DLL/GeneralTweaksGui.cpp
@@ -99,5 +99,81 @@ void GeneralTweaksGui::Draw(const char* Title)
 	ImGui::SameLine();
 	ImGui::Checkbox("##Player max move speed checkbox", &NeedCustomMaxMoveSpeedSet);
 
+	if (ImGui::Button("Print all zone names"))
+	{
+		if (GlobalRfgWorldPtr)
+		{
+			std::cout << "\nPrinting all world zone names...";
+			for (int i = 0; i < GlobalRfgWorldPtr->num_territory_zones; i++)
+			{
+				if (GlobalRfgWorldPtr->global_zone_grid[i] != nullptr && GlobalRfgWorldPtr->global_zone_grid[i] != NULL)
+				{
+					std::cout << "Index: " << i << ", Zone name: " << GlobalRfgWorldPtr->global_zone_grid[i]->name << "\n";
+				}
+				else
+				{
+					std::cout << "The zone pointer at index " << i << " is null\n";
+				}
+			}
+		}
+		else
+		{
+			Logger::Log("Could not print zone names, the world pointer is null!", LogError);
+		}
+	}
+	if (ImGui::Button("Print misc world values"))
+	{
+		std::cout << "\nPrinting all misc world values...";
+		if (GlobalRfgWorldPtr)
+		{
+			std::cout << "Max world objects: " << GlobalRfgWorldPtr->max_world_objects << "\n";
+			std::cout << "Tech level: " << GlobalRfgWorldPtr->tech_level << "\n";
+			std::cout << "Tech level max: " << GlobalRfgWorldPtr->tech_level_max << "\n";
+			std::cout << "Pending filename: " << GlobalRfgWorldPtr->pending_filename << "\n";
+			std::cout << "GlobalRfgWorldPtr->all_objects.array_size: " << GlobalRfgWorldPtr->all_objects.array_size << "\n";
+			std::cout << "GlobalRfgWorldPtr->all_objects.num: " << GlobalRfgWorldPtr->all_objects.num << "\n";
+			std::cout << "Num territory zones: " << GlobalRfgWorldPtr->num_territory_zones << "\n";
+		}
+		else
+		{
+			Logger::Log("Could not print misc world values, the world pointer is null!", LogError);
+		}
+	}
+	if (ImGui::Button("Dump all object info"))
+	{
+		if (GlobalRfgWorldPtr)
+		{
+			std::cout << "\nDumping all object info to ObjectInfoDump.txt...\n";
+			std::ofstream PositionsDump(GetEXEPath(false) + "RFGR Script Loader/ObjectInfoDump.txt", std::ios_base::trunc);
+			PositionsDump << "Start of object info dump...\n";
+			PositionsDump << "Objects array capacity: " << GlobalRfgWorldPtr->all_objects.array_size << "\n";
+			PositionsDump << "Objects array objects contained: " << GlobalRfgWorldPtr->all_objects.num << "\n\n";
+			for (int i = 0; i < GlobalRfgWorldPtr->all_objects.num; i++)
+			{
+				if (&*GlobalRfgWorldPtr->all_objects.elt[i] != NULL && &*GlobalRfgWorldPtr->all_objects.elt[i] != nullptr)
+				{
+					PositionsDump << "Index: " << i << "\n";
+					PositionsDump << "Name: " << world_get_object_name(GlobalRfgWorldPtr, NULL, &*GlobalRfgWorldPtr->all_objects.elt[i]) << "\n";
+					PositionsDump << "Type: " << (int)(*GlobalRfgWorldPtr->all_objects.elt[i]).ObjectType << "\n";
+					PositionsDump << "Subtype: " << (int)(*GlobalRfgWorldPtr->all_objects.elt[i]).SubType << "\n";
+					PositionsDump << "Position:\n";
+					PositionsDump << "    x: " << (*GlobalRfgWorldPtr->all_objects.elt[i]).Position.x << "\n";
+					PositionsDump << "    y: " << (*GlobalRfgWorldPtr->all_objects.elt[i]).Position.y << "\n";
+					PositionsDump << "    z: " << (*GlobalRfgWorldPtr->all_objects.elt[i]).Position.z << "\n\n";
+				}
+				else
+				{
+					PositionsDump << "Skipping object at index " << i << ", invalid pointer.\n";
+				}
+			}
+			PositionsDump << "End of object info dump.";
+			std::cout << "Finished dumped all object into to ObjectInfoDump.txt!\n";
+			PositionsDump.close();
+		}
+		else
+		{
+			Logger::Log("Could not print zone names, the world pointer is null!", LogError);
+		}
+	}
 	ImGui::End();
 }
