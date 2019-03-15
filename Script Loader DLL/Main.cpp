d0d6700b0f32fb3a935b2ad12c9ba39313b8d909
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-03-14 21:40:27 -0400

    Changed welcome menu
    Added words to each button and made the font smaller. This change was made to make it more obvious what each button does for a new user.

--- Script Loader DLL/Main.cpp
+++ Script Loader DLL/Main.cpp
@@ -12,120 +12,122 @@
 
 DWORD WINAPI MainThread(HMODULE hModule)
 {
-	MainModule = hModule;
+    MainModule = hModule;
 
-	try
-	{
-		Logger::Init(LogAll, GetEXEPath(false) + "RFGR Script Loader/Logs/", 10000);
-		Logger::OpenLogFile("Load Log.txt", LogAll, std::ios_base::trunc);
-		Logger::Log("RFGR Script Loader started. Activating.", LogInfo, true);
-	}
-	catch (std::exception& Ex)
-	{
-		//MessageBoxA(FindTopWindow(GetProcessID("rfg.exe")), "Exception detected during Logger initialization. Will attempt to use secondary logging location...", "Logger failed to initialize!", MB_OK);
-		std::string MessageBoxString = "Exception detected during Logger initialization! Please show this to the current script loader maintainer. It's much harder to fix any further problems which might occur without logs. \n";
-		MessageBoxString += Ex.what();
-		MessageBoxA(FindTopWindow(GetProcessID("rfg.exe")), MessageBoxString.c_str(), "Logger failed to initialize!", MB_OK);
-		Logger::CloseAllLogFiles();
-	}
+    try
+    {
+        Logger::Init(LogAll, GetEXEPath(false) + "RFGR Script Loader/Logs/", 10000);
+        Logger::OpenLogFile("Load Log.txt", LogAll, std::ios_base::trunc);
+        Logger::Log("RFGR Script Loader started. Activating.", LogInfo, true);
+    }
+    catch (std::exception& Ex)
+    {
+        //MessageBoxA(FindTopWindow(GetProcessID("rfg.exe")), "Exception detected during Logger initialization. Will attempt to use secondary logging location...", "Logger failed to initialize!", MB_OK);
+        std::string MessageBoxString = "Exception detected during Logger initialization! Please show this to the current script loader maintainer. It's much harder to fix any further problems which might occur without logs. \n";
+        MessageBoxString += Ex.what();
+        MessageBoxA(FindTopWindow(GetProcessID("rfg.exe")), MessageBoxString.c_str(), "Logger failed to initialize!", MB_OK);
+        Logger::CloseAllLogFiles();
+    }
 
-	ProgramManager Program(hModule);
-	try
-	{
-		if (!Program.LoadDataFromConfig())
-		{
-			FreeLibraryAndExitThread(hModule, 0);
-			return 0;
-		}
-		Program.OpenConsole();
-		Program.SetMemoryLocations();
-		Program.Initialize();
-	}
-	catch(std::exception& Ex)
-	{
-		std::string MessageBoxString = "Exception detected during script loader initialization! Please provide this and a zip file with your logs folder (./RFGR Script Loader/Logs/) to the maintainer. \n";
-		MessageBoxString += Ex.what();
-		MessageBoxA(FindTopWindow(GetProcessID("rfg.exe")), MessageBoxString.c_str(), "Script loader failed to initialize!", MB_OK);
-	}
+    ProgramManager Program(hModule);
+    try
+    {
+        if (!Program.LoadDataFromConfig())
+        {
+            FreeLibraryAndExitThread(hModule, 0);
+            return 0;
+        }
+        Program.OpenConsole();
+        Program.SetMemoryLocations();
+        Program.Initialize();
+    }
+    catch(std::exception& Ex)
+    {
+        std::string MessageBoxString = "Exception detected during script loader initialization! Please provide this and a zip file with your logs folder (./RFGR Script Loader/Logs/) to the maintainer. \n";
+        MessageBoxString += Ex.what();
+        Logger::Log(MessageBoxString, LogFatalError, true, true);
+        MessageBoxA(FindTopWindow(GetProcessID("rfg.exe")), MessageBoxString.c_str(), "Script loader failed to initialize!", MB_OK);
+    }
 
-	try
-	{
-		Logger::Log("RFGR script loader successfully activated.", LogInfo, true);
-		Logger::CloseLogFile("Load Log.txt");
+    try
+    {
+        Logger::Log("RFGR script loader successfully activated.", LogInfo, true);
+        Logger::CloseLogFile("Load Log.txt");
 
-		Logger::OpenLogFile("General Log.txt", LogInfo, std::ios_base::trunc);
-		Logger::OpenLogFile("Error Log.txt", LogWarning | LogError | LogFatalError, std::ios_base::trunc);
-		Logger::OpenLogFile("Json Log.txt", LogJson, std::ios_base::trunc);
-		Logger::OpenLogFile("Lua Log.txt", LogLua, std::ios_base::trunc);
-	}
-	catch(std::exception& Ex)
-	{
-		std::string MessageBoxString = "Exception detected when opening the default log files. Please show this to the current script loader maintainer. It's much harder to fix any further problems which might occur without logs. \n";
-		MessageBoxString += Ex.what();
-		MessageBoxA(FindTopWindow(GetProcessID("rfg.exe")), MessageBoxString.c_str(), "Logger failed to open default log files!", MB_OK);
-	}
+        Logger::OpenLogFile("General Log.txt", LogInfo, std::ios_base::trunc);
+        Logger::OpenLogFile("Error Log.txt", LogWarning | LogError | LogFatalError, std::ios_base::trunc);
+        Logger::OpenLogFile("Json Log.txt", LogJson, std::ios_base::trunc);
+        Logger::OpenLogFile("Lua Log.txt", LogLua, std::ios_base::trunc);
+    }
+    catch(std::exception& Ex)
+    {
+        std::string MessageBoxString = "Exception detected when opening the default log files. Please show this to the current script loader maintainer. It's much harder to fix any further problems which might occur without logs. \n";
+        MessageBoxString += Ex.what();
+        Logger::Log(MessageBoxString, LogFatalError, true, true);
+        MessageBoxA(FindTopWindow(GetProcessID("rfg.exe")), MessageBoxString.c_str(), "Logger failed to open default log files!", MB_OK);
+    }
 
-	Program.Scripts.ScanScriptsFolder();
+    Program.Scripts.ScanScriptsFolder();
 
-	GameState RFGRState;
-	while (!Program.ShouldClose()) //Todo: Change to respond to PostQuitMessage(0) in WndProc;
-	{
-		/*The error messages in the next three if statements are BS. They really
-		detect if the player has entered a multiplayer mode. I changed them to 
-		hopefully thwart anyone trying to disable multiplayer checks with binary
-		patches. It likely won't slow down people who know what they are doing, 
-		but I figure it's worth a go.*/
-		RFGRState = GameseqGetState();
-		if (RFGRState == GS_WRECKING_CREW_MAIN_MENU || 
-			RFGRState == GS_WRECKING_CREW_CHARACTER_SELECT || 
-			RFGRState == GS_WRECKING_CREW_SCOREBOARD || 
-			RFGRState == GS_MULTIPLAYER_LIVE || 
-			RFGRState == GS_WC_INIT || 
-			RFGRState == GS_WC_SHUTDOWN || 
-			RFGRState == GS_MULTIPLAYER_LIVE_FIND_SERVERS)
-		{
-			//MessageBoxA(FindTopWindow(GetProcessID("rfg.exe")), "MP usage detected, shutting down!", "Multiplayer mode detected", MB_OK);
- 			Logger::Log("Error in UI hook, script loader crashing!", LogFatalError, true);
-			FreeLibraryAndExitThread(hModule, 0);
-			return 0;
-		}
-		if (*(bool*)InMultiplayer)
-		{
-			//MessageBoxA(FindTopWindow(GetProcessID("rfg.exe")), "MP usage detected, shutting down!", "Multiplayer mode detected", MB_OK);
-			Logger::Log("Invalid graphics state in script loader overlay, crashing!", LogFatalError, true);
-			FreeLibraryAndExitThread(hModule, 0);
-			return 0;
-		}
-		if (MultiplayerHookTriggered)
-		{
-			//MessageBoxA(FindTopWindow(GetProcessID("rfg.exe")), "MP usage detected, shutting down!", "Multiplayer mode detected", MB_OK);
-			Logger::Log("Null pointer in script loader callback system, crashing!", LogFatalError, true);
-			FreeLibraryAndExitThread(hModule, 0);
-			return 0;
-		}
-		Program.ProcessInput();
-		Program.Update();
-	}
-	Program.CloseConsole();
-	Program.Exit();
+    GameState RFGRState;
+    while (!Program.ShouldClose()) //Todo: Change to respond to PostQuitMessage(0) in WndProc;
+    {
+        /*The error messages in the next three if statements are BS. They really
+        detect if the player has entered a multiplayer mode. I changed them to 
+        hopefully thwart anyone trying to disable multiplayer checks with binary
+        patches. It likely won't slow down people who know what they are doing, 
+        but I figure it's worth a go.*/
+        RFGRState = GameseqGetState();
+        if (RFGRState == GS_WRECKING_CREW_MAIN_MENU || 
+            RFGRState == GS_WRECKING_CREW_CHARACTER_SELECT || 
+            RFGRState == GS_WRECKING_CREW_SCOREBOARD || 
+            RFGRState == GS_MULTIPLAYER_LIVE || 
+            RFGRState == GS_WC_INIT || 
+            RFGRState == GS_WC_SHUTDOWN || 
+            RFGRState == GS_MULTIPLAYER_LIVE_FIND_SERVERS)
+        {
+            //MessageBoxA(FindTopWindow(GetProcessID("rfg.exe")), "MP usage detected, shutting down!", "Multiplayer mode detected", MB_OK);
+            Logger::Log("Error in UI hook, script loader crashing!", LogFatalError, true);
+            FreeLibraryAndExitThread(hModule, 0);
+            return 0;
+        }
+        if (*(bool*)InMultiplayer)
+        {
+            //MessageBoxA(FindTopWindow(GetProcessID("rfg.exe")), "MP usage detected, shutting down!", "Multiplayer mode detected", MB_OK);
+            Logger::Log("Invalid graphics state in script loader overlay, crashing!", LogFatalError, true);
+            FreeLibraryAndExitThread(hModule, 0);
+            return 0;
+        }
+        if (MultiplayerHookTriggered)
+        {
+            //MessageBoxA(FindTopWindow(GetProcessID("rfg.exe")), "MP usage detected, shutting down!", "Multiplayer mode detected", MB_OK);
+            Logger::Log("Null pointer in script loader callback system, crashing!", LogFatalError, true);
+            FreeLibraryAndExitThread(hModule, 0);
+            return 0;
+        }
+        Program.ProcessInput();
+        Program.Update();
+    }
+    Program.CloseConsole();
+    Program.Exit();
 
-	Logger::Log("RFGR script loader deactivated", LogInfo, true);
-	Logger::CloseAllLogFiles();
+    Logger::Log("RFGR script loader deactivated", LogInfo, true);
+    Logger::CloseAllLogFiles();
 
-	FreeLibraryAndExitThread(hModule, 0);
-	return 0;
+    FreeLibraryAndExitThread(hModule, 0);
+    return 0;
 }
 
 BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 {
-	//DisableThreadLibraryCalls(hModule);
-	switch (dwReason)
-	{
-	case DLL_PROCESS_ATTACH:
-		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)MainThread, hModule, 0, 0);
-		break;
-	default:
-		break;
-	}
-	return TRUE;
+    //DisableThreadLibraryCalls(hModule);
+    switch (dwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)MainThread, hModule, 0, 0);
+        break;
+    default:
+        break;
+    }
+    return TRUE;
 }
--- Script Loader DLL/WelcomeGui.cpp
+++ Script Loader DLL/WelcomeGui.cpp
@@ -40,56 +40,58 @@ void WelcomeGui::Draw(const char* Title)
 		return;
 	}
 
-	if (Gui.ShowAppMetrics) { ImGui::ShowMetricsWindow(&Gui.ShowAppMetrics); }
-	if (Gui.ShowAppAbout) { ShowAboutWindow(&Gui.ShowAppAbout); }
-
 	//ImGui::PushStyleVar(ImGuiStyleVar_FrameBorderSize, 0.0f);
 	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4());
 	//ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4());
 	//ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4());
-	ImGui::PushFont(FontHuge);
+	ImGui::PushFont(FontLarge);
 	ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.556f, 0.823f, 0.541f, 1.0f));
-	if (ImGui::Button(std::string(std::string(ICON_FA_CODE) + u8"##ScriptSelectIcon").c_str()))
+	if (ImGui::Button(std::string(u8"Scripts " + std::string(ICON_FA_CODE) + u8"##ScriptSelectIcon").c_str()))
 	{
 		Gui.ShowAppScriptsMenu = !Gui.ShowAppScriptsMenu;
 	}
 	ImGui::PopStyleColor();
 	Utilities::GUI::TooltipOnPrevious("This menu displays all the scripts detected in the scripts folder and lets you run, edit, and stop them on command. If the script isn't using event hooks then stopping it does nothing, since it stops after running once.", FontNormal);
-	ImGui::SameLine();
+	
+	//ImGui::SameLine();
 	ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.357f, 0.659f, 0.863f, 1.0f));
-	if (ImGui::Button(std::string(std::string(ICON_FA_TERMINAL) + u8"##LuaConsoleIcon").c_str()))
+	if (ImGui::Button(std::string(u8"Lua console " + std::string(ICON_FA_TERMINAL) + u8"##LuaConsoleIcon").c_str()))
 	{
 		Gui.ToggleLuaConsole();
 	}
 	ImGui::PopStyleColor();
 	Utilities::GUI::TooltipOnPrevious("The console is useful for quickly setting values or calling functions without writing a script. Anything that scripts have access to, the console does too. The console is just running your input into it as a script.", FontNormal);
-	ImGui::SameLine();
+	
+	//ImGui::SameLine();
 	ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 0.808f, 0.267f, 1.0f));
-	if (ImGui::Button(std::string(std::string(ICON_FA_EDIT) + u8"##ScriptEditorIcon").c_str()))
+	if (ImGui::Button(std::string(u8"Script editor " + std::string(ICON_FA_EDIT) + u8"##ScriptEditorIcon").c_str()))
 	{
 		Gui.ShowAppScriptEditor = !Gui.ShowAppScriptEditor;
 	}
 	ImGui::PopStyleColor();
 	Utilities::GUI::TooltipOnPrevious("The script editor allows you to save, load, edit, and run lua scripts in game, and provides basic lua syntax highlighting.", FontNormal);
-	ImGui::SameLine();
+	
+	//ImGui::SameLine();
 	ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.404f, 0.416f, 0.435f, 1.0f));
-	if (ImGui::Button(std::string(std::string(ICON_FA_SLIDERS_H) + u8"##TweaksIcon").c_str()))
+	if (ImGui::Button(std::string(u8"Tweaks menu " + std::string(ICON_FA_SLIDERS_H) + u8"##TweaksIcon").c_str()))
 	{
 		Gui.ShowAppTweaksMenu = !Gui.ShowAppTweaksMenu;
 	}
 	ImGui::PopStyleColor();
 	Utilities::GUI::TooltipOnPrevious("This menu has settings for invulnerability, infinite jetpack, player move speed, player jump height, xray vision, and more.", FontNormal);
-	ImGui::SameLine();
+	
+	//ImGui::SameLine();
 	ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.961f, 0.753f, 0.698f, 1.0f));
-	if (ImGui::Button(std::string(std::string(ICON_FA_MAP_MARKED) + u8"##TeleportIcon").c_str()))
+	if (ImGui::Button(std::string(u8"Teleport menu " + std::string(ICON_FA_MAP_MARKED) + u8"##TeleportIcon").c_str()))
 	{
 		Gui.ShowAppTeleportMenu = !Gui.ShowAppTeleportMenu;
 	}
 	ImGui::PopStyleColor();
 	Utilities::GUI::TooltipOnPrevious("This menu allows you to teleport around the map. There are many preset locations, and you may create your own. Any of your custom locations are saved in TeleportLocations.json. You don't need to edit it by hand as you can do all the editing you need in the gui.", FontNormal);
-	ImGui::SameLine();
+	
+	//ImGui::SameLine();
 	ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.361f, 0.129f, 1.0f, 1.0f));
-	if (ImGui::Button(std::string(std::string(ICON_FA_PALETTE) + u8"##ThemeEditorIcon").c_str()))
+	if (ImGui::Button(std::string(u8"Theme editor " + std::string(ICON_FA_PALETTE) + u8"##ThemeEditorIcon").c_str()))
 	{
 		Gui.ShowAppThemeEditor = !Gui.ShowAppThemeEditor;
 	}
@@ -115,6 +117,8 @@ the top menu bar above.");
 	ImGui::SameLine(); ImGui::TextColored(SecondaryTextColor, "Numpad 1");
 	ImGui::Text("Toggle fog: ");
 	ImGui::SameLine(); ImGui::TextColored(SecondaryTextColor, "Numpad 2");
+	ImGui::Text("Toggle free cam: ");
+	ImGui::SameLine(); ImGui::TextColored(SecondaryTextColor, "Numpad 3");
 
 	/*if (ImGui::CollapsingHeader("Notes:"))
 	{
