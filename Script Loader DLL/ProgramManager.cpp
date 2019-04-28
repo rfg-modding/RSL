dfb402282e8a5ab350c8946806784def7756a26c
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-04-27 21:09:56 -0400

    Slight adjustment to health reset on script loader unload

--- Script Loader DLL/ProgramManager.cpp
+++ Script Loader DLL/ProgramManager.cpp
@@ -120,7 +120,7 @@ void ProgramManager::Exit()
 	if (Globals::PlayerPtr)
 	{
 		Globals::PlayerPtr->Flags.invulnerable = false;
-		Globals::PlayerPtr->HitPoints = 120.0f;
+		Globals::PlayerPtr->HitPoints = 230.0f;
 	}
 
 	Beep(900, 100);
