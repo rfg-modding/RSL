3ac4d92f3e3695c02c8eb86d0168676839281ccf
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-05-03 01:15:25 -0400

    More ScriptManager comments

--- Script Loader DLL/ScriptManager.h
+++ Script Loader DLL/ScriptManager.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "LuaFunctions.h"
 
+/* Contains basic information about a script to avoid duplicate calculations.
+ * This includes it's full path, folder path, and file name. Currently does 
+ * not buffer the script itself. Scripts are only loaded at the moment that
+ * they are run.
+ */
 class Script
 {
 public:
@@ -8,11 +13,17 @@ public:
 	Script(std::string FullPath_, std::string FolderPath_, std::string Name_) : FullPath(FullPath_), FolderPath(FolderPath_), Name(Name_) { };
 	~Script() = default;;
 
-	std::string FullPath; //Full path with name.
-	std::string FolderPath; //Full path without name.
-	std::string Name; //Script name.
+	std::string FullPath; //Full path with file name.
+	std::string FolderPath; //Full path without file name.
+	std::string Name; //Script file name.
 };
 
+/* This class manages the script loader lua state and scripting. This includes binding
+ * rfg structures and functions to lua, and finding and running lua scripts. It contains
+ * functions for detecting valid scripts in folders, and safely running scripts without 
+ * worry of crashing the game (generally). See ScriptManager.cpp for more information
+ * about individual member functions.
+ */
 class ScriptManager
 {
 public:
@@ -30,8 +41,8 @@ public:
 
 	bool RunStringAsScript(std::string Buffer, std::string Name);
 
-	sol::state* LuaState = nullptr; //Use a pointer for easy LuaState resets.
-	std::vector <Script> Scripts;
+	sol::state* LuaState = nullptr; //Uses a pointer for easy LuaState resets.
+	std::vector <Script> Scripts; //List of scripts detected in Scripts folder on the last scan.
 
 private:
 	std::string GetScriptNameFromPath(std::string FullPath) const;
