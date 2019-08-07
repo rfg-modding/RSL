3808c193f6e1dd752580b1c593c0307becd403d7
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-08-07 12:41:33 -0400

    Update build scripts
    Remove unnecessary variable. Fix version string formatting.

--- Tools/Appveyor_Pre_Build_Script.ps1
+++ Tools/Appveyor_Pre_Build_Script.ps1
@@ -1,3 +1,3 @@
 
-$rsl_version_string = "$env:APPVEYOR_BUILD_VERSION-latest"
-Set-Content -Path '.\RSL\Version.h' -Value "#pragma once`n`nconstexpr auto RSL_VERSION = `"$rsl_version_string`";"
+Set-Content -Path '.\RSL\Version.h' -Value "#pragma once`n`nconstexpr auto RSL_VERSION = `"$env:APPVEYOR_BUILD_VERSION`";"
+Get-Content '.\RSL\Version.h'
