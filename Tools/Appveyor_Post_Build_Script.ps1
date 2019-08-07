ff1d21694914ef8392051d0d0ed5ae336b1fdc8c
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-08-07 14:22:13 -0400

    Update appveyor post-build script
    Made it shorter and more concise. Removed some extra code used when testing out appveyor that is no longer needed.

--- Tools/Appveyor_Post_Build_Script.ps1
+++ Tools/Appveyor_Post_Build_Script.ps1
@@ -1,24 +1,14 @@
 mkdir ".\Red Faction Guerrilla Re-mars-tered"
 mkdir ".\Red Faction Guerrilla Re-mars-tered\RSL"
 
-Copy-Item ".\Release\dinput8.dll" -Destination ".\Red Faction Guerrilla Re-mars-tered"
-Copy-Item ".\Release\RSL.dll" -Destination ".\Red Faction Guerrilla Re-mars-tered\RSL"
-Copy-Item ".\Core" -Destination ".\Red Faction Guerrilla Re-mars-tered\RSL\Core" -Recurse
-Copy-Item ".\Content\Scripts" -Destination ".\Red Faction Guerrilla Re-mars-tered\RSL\Scripts" -Recurse
-Copy-Item ".\Content\Fonts" -Destination ".\Red Faction Guerrilla Re-mars-tered\RSL\Fonts" -Recurse
-Copy-Item ".\Content\Settings" -Destination ".\Red Faction Guerrilla Re-mars-tered\RSL\Settings" -Recurse
+Copy-Item ".\Release\dinput8.dll" ".\Red Faction Guerrilla Re-mars-tered"
+Copy-Item ".\Release\RSL.dll" ".\Red Faction Guerrilla Re-mars-tered\RSL"
+Copy-Item ".\Core" ".\Red Faction Guerrilla Re-mars-tered\RSL\Core" -Recurse
+Copy-Item ".\Content\*" ".\Red Faction Guerrilla Re-mars-tered\RSL\" -Recurse
 
-Copy-Item ".\LICENSE" -Destination ".\Red Faction Guerrilla Re-mars-tered\RSL"
-Copy-Item ".\readme.md" -Destination ".\Red Faction Guerrilla Re-mars-tered\RSL"
+Copy-Item ".\LICENSE" ".\Red Faction Guerrilla Re-mars-tered\RSL"
+Copy-Item ".\readme.md" ".\Red Faction Guerrilla Re-mars-tered\RSL"
 
 cmd /c 7z a "$env:APPVEYOR_BUILD_VERSION.zip" ".\Red Faction Guerrilla Re-mars-tered"
 
-cmd /c dir
-
-$build_version = $env:APPVEYOR_BUILD_VERSION
-$zip_name = "$build_version.zip"
-
-Write-Host $build_version
-Write-Host $zip_name
-
-Push-AppveyorArtifact $zip_name
+Push-AppveyorArtifact "$env:APPVEYOR_BUILD_VERSION.zip"
