106973aec12b6091cd971bef5c800df1f9d5994d
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-08-07 20:39:11 -0400

    Add Azure specific build scripts
    Messing around with different CI platforms to see which ones best. Going to keep Appveyor up for now and test Azure as well.

---
+++ Tools/AzurePostBuild.ps1
@@ -0,0 +1,10 @@
+mkdir ".\Red Faction Guerrilla Re-mars-tered"
+mkdir ".\Red Faction Guerrilla Re-mars-tered\RSL"
+
+Copy-Item ".\Release\dinput8.dll" ".\Red Faction Guerrilla Re-mars-tered"
+Copy-Item ".\Release\RSL.dll" ".\Red Faction Guerrilla Re-mars-tered\RSL"
+Copy-Item ".\Core" ".\Red Faction Guerrilla Re-mars-tered\RSL\Core" -Recurse
+Copy-Item ".\Content\*" ".\Red Faction Guerrilla Re-mars-tered\RSL\" -Recurse
+
+Copy-Item ".\LICENSE" ".\Red Faction Guerrilla Re-mars-tered\RSL"
+Copy-Item ".\readme.md" ".\Red Faction Guerrilla Re-mars-tered\RSL"
---
+++ Tools/AzurePreBuild.ps1
@@ -0,0 +1,3 @@
+
+Set-Content -Path '.\RSL\Version.h' -Value "#pragma once`n`nconstexpr auto RSL_VERSION = `"$env:APPVEYOR_BUILD_VERSION`";"
+Get-Content '.\RSL\Version.h'
