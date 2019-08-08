3bfbf1df6823d2d7b66c574158affb95efd3a67e
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-08-07 21:14:33 -0400

    AzurePreBuild.ps1

--- Tools/AzurePreBuild.ps1
+++ Tools/AzurePreBuild.ps1
@@ -1,5 +1,6 @@
 
-cmd /c dir
+Write-Host $env:BuildID
+Write-Host $env:Build.BuildID
 
-Set-Content -Path '.\RSL\Version.h' -Value "#pragma once`n`nconstexpr auto RSL_VERSION = `"$env:BuildID`";"
+Set-Content -Path '.\RSL\Version.h' -Value "#pragma once`n`nconstexpr auto RSL_VERSION = `"$env:Build.BuildID`";"
 Get-Content '.\RSL\Version.h'
