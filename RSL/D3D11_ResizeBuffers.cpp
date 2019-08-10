a5ef3cbb128708b36ddf325c742626b12ec33659
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-08-10 11:03:27 -0400

    Fix resize crash the only occurs during RSL init
    Turns out you could still crash the game by resizing the screen during the RSL init process. The hook would try to access values like Globals::GraphicsSystemPtr while they were nullptrs. Simply extend one of the earlier checks to ensure that the resize code isn't run unless the script loader has inited the necessary values.

--- RSL/D3D11_ResizeBuffers.cpp
+++ RSL/D3D11_ResizeBuffers.cpp
@@ -3,10 +3,12 @@
 
 HRESULT __stdcall Hooks::D3D11_ResizeBuffersHook(IDXGISwapChain* pSwapChain, UINT BufferCount, UINT Width, UINT Height, DXGI_FORMAT NewFormat, UINT SwapChainFlags)
 {
+    bool ShouldReInit = false;
     if (Globals::MainRenderTargetView)
     {
         Globals::MainRenderTargetView->Release();
         Globals::MainRenderTargetView = nullptr;
+        ShouldReInit = true;
     }
 
     HRESULT Result = D3D11_ResizeBuffersFuncPtr(pSwapChain, BufferCount, Width, Height, NewFormat, SwapChainFlags);
@@ -14,27 +16,30 @@ HRESULT __stdcall Hooks::D3D11_ResizeBuffersHook(IDXGISwapChain* pSwapChain, UIN
     Logger::Log("[D3D11_ResizeBuffersHook]:: BufferCount: {}, Width: {}, Height: {}, NewFormat: {}, SwapChainFlags: {} .... Result: {:#x}\n", BufferCount, Width, Height, NewFormat, SwapChainFlags, (uint)Result);
 #endif
 
-    Globals::D3D11Device = Globals::GraphicsSystemPtr->pDevice;
-    Globals::D3D11Context = Globals::GraphicsSystemPtr->pImmediateContext;
-    Globals::D3D11SwapchainPtr = Globals::GraphicsSystemPtr->pDefaultSwapChain->pSwapChain;
-
-    ID3D11Texture2D* BackBuffer;
-    Result = Globals::D3D11SwapchainPtr->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<LPVOID*>(&BackBuffer));
-    if (Result != S_OK)
+    if (ShouldReInit)
     {
-        Logger::LogFatalError("GetBuffer() failed, return value: {}\n", Result);
-    }
+        Globals::D3D11Device = Globals::GraphicsSystemPtr->pDevice;
+        Globals::D3D11Context = Globals::GraphicsSystemPtr->pImmediateContext;
+        Globals::D3D11SwapchainPtr = Globals::GraphicsSystemPtr->pDefaultSwapChain->pSwapChain;
 
-    D3D11_RENDER_TARGET_VIEW_DESC desc = {};
-    memset(&desc, 0, sizeof(desc));
-    desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; //Required to avoid rendering issue with overlay. Without this the proper rgb values will not be displayed.
-    desc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
-    Result = Globals::D3D11Device->CreateRenderTargetView(BackBuffer, &desc, &Globals::MainRenderTargetView);
-    if (Result != S_OK)
-    {
-        Logger::LogFatalError("CreateRenderTargetView() failed, return value: {}\n", Result);
+        ID3D11Texture2D* BackBuffer;
+        Result = Globals::D3D11SwapchainPtr->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<LPVOID*>(&BackBuffer));
+        if (Result != S_OK)
+        {
+            Logger::LogFatalError("GetBuffer() failed, return value: {}\n", Result);
+        }
+
+        D3D11_RENDER_TARGET_VIEW_DESC desc = {};
+        memset(&desc, 0, sizeof(desc));
+        desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; //Required to avoid rendering issue with overlay. Without this the proper rgb values will not be displayed.
+        desc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+        Result = Globals::D3D11Device->CreateRenderTargetView(BackBuffer, &desc, &Globals::MainRenderTargetView);
+        if (Result != S_OK)
+        {
+            Logger::LogFatalError("CreateRenderTargetView() failed, return value: {}\n", Result);
+        }
+        BackBuffer->Release();
     }
-    BackBuffer->Release();
 
     return Result;
 }
