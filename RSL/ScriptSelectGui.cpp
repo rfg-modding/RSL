a10f99b897da06212660891a4120125063e4b209
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-08-12 09:09:53 -0400

    Increase the default scripts gui size to show all the buttons
    Previously couldn't see the "edit" button without resizing the gui. Many people probably wouldn't ever see it because of this.

--- RSL/ScriptSelectGui.cpp
+++ RSL/ScriptSelectGui.cpp
@@ -9,7 +9,7 @@ void ScriptSelectGui::Draw()
 	{
 		return;
 	}
-	ImGui::SetNextWindowSize(ImVec2(400.0f, 500.0f), ImGuiCond_FirstUseEver);
+	ImGui::SetNextWindowSize(ImVec2(450.0f, 500.0f), ImGuiCond_FirstUseEver);
 	if (!ImGui::Begin(Title.c_str(), &Visible, WindowFlags))
 	{
 		ImGui::End();
