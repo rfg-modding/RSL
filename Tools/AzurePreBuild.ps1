6b44b902fe64bcd0992a57e4d09091461e6f975a
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-08-13 21:56:54 -0400

    Update azure pipelines files
    Still trying to fix the build errors on azure...

--- Tools/AzurePreBuild.ps1
+++ Tools/AzurePreBuild.ps1
@@ -1,5 +1,3 @@
 
-Write-Host $BuildID
-
-Set-Content -Path '.\RSL\Version.h' -Value "#pragma once`n`nconstexpr auto RSL_VERSION = `"$env:BuildID`";"
+Set-Content -Path '.\RSL\Version.h' -Value "#pragma once`n`nconstexpr auto RSL_VERSION = `"$env:RSL_VERSION`";"
 Get-Content '.\RSL\Version.h'
