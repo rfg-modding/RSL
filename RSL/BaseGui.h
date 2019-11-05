8864986934047058a8956f4ddca37576c856fa3d
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-11-04 20:08:51 -0500

    More changes to BaseGui

--- RSL/BaseGui.h
+++ RSL/BaseGui.h
@@ -4,15 +4,21 @@
 class BaseGui
 {
 public:
-	virtual ~BaseGui() {};
-	virtual void Draw();
-	
-	std::string Title;
+    //Disable copying by deleting the copy constructor and copy assignment constructor to avoid derived class slicing
+    BaseGui(BaseGui const&) = delete;
+    BaseGui& operator=(BaseGui const&) = delete;
+    virtual ~BaseGui() { }
+    virtual void Draw() = 0;
+
+    std::string Title;
 
     void Show() { Visible = true; }
     void Hide() { Visible = false; }
     void Toggle() { Visible = !Visible; }
     [[nodiscard]] bool IsVisible() const { return Visible; }
 
-	bool Visible = false;
+    bool Visible = false;
+
+protected:
+    BaseGui() { }
 };
