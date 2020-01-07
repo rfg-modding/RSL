cde704fb9c158b3304fa21fb543d4e78f93e029d
moneyl <8206401+Moneyl@users.noreply.github.com>
2020-01-07 00:20:45 -0500

    Fix bug that completely broke the games event system
    Such a major bug, only one line of code difference.

--- RSL/gem_post_event.cpp
+++ RSL/gem_post_event.cpp
@@ -4,6 +4,6 @@ void __cdecl Hooks::gem_post_event_hook(event_data& data)
 {
 #if DEBUG
     Logger::LogWarning("[gem_post_event_hook]:: Rfg event posted. Type: {}\n", magic_enum::enum_name(data.type));
-    return rfg::gem_post_event(data);
 #endif
+    return rfg::gem_post_event(data);
 }
